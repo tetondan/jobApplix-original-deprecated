7a79fd1c4c365660e037eecd6e6d35e27d053974
tetondan <dfrehner1@gmail.com>
2016-09-16 14:54:37 -0600

    additional styling to image upload page

--- client/app/controllers/imageUploadController.js
+++ client/app/controllers/imageUploadController.js
@@ -4,16 +4,19 @@ angular.module('myApp.imageUploadCont', [])
     $scope.continue = function(){
       $state.go('dashboard.setup')
     };
+    $scope.uploading = false;
     $scope.thumbnail = {dataUrl: '/no_image.png'};
-    $scope.filecallout = function(){
+    $scope.fileUpload = function(){
       if($scope.file && $scope.file.size > 2000000) {
         alert('Sorry, file size must be under 2MB');
         return false;
       } else if ($scope.file){
+        $scope.uploading = true;
         BusinessDataServices.gets3signature($scope.file)
           .then(function(data){
             BusinessDataServices.s3upload($scope.file, data.signedRequest, data.url)
             .then(function(){
+              $scope.uploading = false;
               $scope.advance = true;
             })
           })
